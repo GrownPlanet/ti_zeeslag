ba16dd5b4c6ffced2c280bd1b9d3bf32b8d04de6
Timo Dehandschutter <timo.dehandschutter@gmail.com>
2025-05-22 21:35:54 +0200

    finished?

--- main.c
+++ main.c
@@ -21,29 +21,32 @@ typedef enum {
     CS_Water, CS_Ship, CS_WaterHit, CS_ShipHit
 } CellState_t;
 
-void get_input(CellState_t map[BOARD_W * BOARD_H]);
-void game_loop(CellState_t p1[BOARD_W * BOARD_H], (CellState_t p2[BOARD_W * BOARD_H);
+void get_input(int player, CellState_t board[BOARD_W * BOARD_H], int ship_len[NUM_SHIPS]);
+void game_loop(CellState_t p1[BOARD_W * BOARD_H], CellState_t p2[BOARD_W * BOARD_H], int totalhits);
 
 int main() {
-    clrscr();
-    printf("Gebruik de pijltjes om\nte bewegen, + om te draaien, en - om te plaatsen\nDruk om door te gaan\n");
-    while (!getk());
-
-    CellState_t p1_map[BOARD_W * BOARD_H] = { 0 };
-    get_input(p1_map);
+    int ship_len[NUM_SHIPS] = { 5, 4, 3, 2, 2 };
+    int totalhits = 16; // sum of ship_len
 
     clrscr();
-    printf("Geef de rekenmachine aan de volgende speler\n");
+    printf("Gebruik de pijltjes om\nte bewegen, + om te draaien, en - om te plaatsen. - toont een mis aan en X een raak shot aan\nDruk om door te gaan\n");
     while (!getk());
 
-    CellState_t p2_map[BOARD_W * BOARD_H] = { 0 };
-    get_input(p2_map);
+    CellState_t p1_board[BOARD_W * BOARD_H] = { 0 };
+    get_input(1, p1_board, ship_len);
+
+    clrscr();
+    printf("Geef de rekenmachine aan speler 2\n");
+    while (!getk());
+
+    CellState_t p2_board[BOARD_W * BOARD_H] = { 0 };
+    get_input(2, p2_board, ship_len);
 
     clrscr();
     printf("Geef de rekenmachine terug aan speler 1\n");
     while (!getk());
 
-    main_loop(p1_map, p2_map);
+    game_loop(p1_board, p2_board, totalhits);
 
     return 0;
 }
@@ -55,12 +58,12 @@ void print_ship(int x, int y, int dir, int len) {
     }
 }
 
-void clear_ship(int x, int y, int dir, int len, CellState_t map[BOARD_W * BOARD_H]) {
+void clear_ship(int x, int y, int dir, int len, CellState_t board[BOARD_W * BOARD_H]) {
     for (int i = 0; i < len; i++) {
         int ax = x+i*(dir==0);
         int ay = y+i*(dir==1);
         gotoxy(ax*2, ay);
-        switch (map[ay*BOARD_W+ax]) {
+        switch (board[ay*BOARD_W+ax]) {
             case CS_Water: printf("."); break;
             case CS_Ship: printf("O"); break;
             default: printf("?"); break;
@@ -68,9 +71,7 @@ void clear_ship(int x, int y, int dir, int len, CellState_t map[BOARD_W * BOARD_
     }
 }
 
-void get_input(CellState_t map[BOARD_W * BOARD_H]) {
-    int ship_len[NUM_SHIPS] = { 5, 4, 3, 2, 2 };
-
+void get_input(int player, CellState_t board[BOARD_W * BOARD_H], int ship_len[NUM_SHIPS]) {
     clrscr();
     for (int x = 0; x < 10; x++) {
         for (int y = 0; y < 10; y++) {
@@ -78,6 +79,8 @@ void get_input(CellState_t map[BOARD_W * BOARD_H]) {
             printf(".");
         }
     }
+    gotoxy(20, 0);
+    printf("P%d", player);
 
     int x = 0;
     int y = 0;
@@ -91,16 +94,16 @@ void get_input(CellState_t map[BOARD_W * BOARD_H]) {
     while (len_i < NUM_SHIPS) {
         int k = getk();
         bool moved = (k == K_UP || k == K_DOWN || k == K_LEFT || k == K_RIGHT || k == K_TURN);
-        if (moved) clear_ship(x, y, dir, len, map);
+        if (moved) clear_ship(x, y, dir, len, board);
         switch (k) {
             case K_EXIT:
                 clrscr();
                 printf("Bye!\n");
                 exit(0);
-            case K_UP:    if (y > 0) y -= 1; break;
-            case K_DOWN:  if (y+(len-1)*(dir==1)+1 < BOARD_H) y += 1; break;
-            case K_LEFT:  if (x > 0) x -= 1; break;
-            case K_RIGHT: if (x+(len-1)*(dir==0)+1 < BOARD_W) x += 1; break;
+            case K_UP:    if (y > 0) y--; break;
+            case K_DOWN:  if (y+(len-1)*(dir==1)+1 < BOARD_H) y++; break;
+            case K_LEFT:  if (x > 0) x--; break;
+            case K_RIGHT: if (x+(len-1)*(dir==0)+1 < BOARD_W) x++; break;
             case K_TURN: {
                 dir = !dir;
                 if (dir == 0 && x+len > BOARD_W) x = BOARD_W - len;
@@ -108,12 +111,22 @@ void get_input(CellState_t map[BOARD_W * BOARD_H]) {
                 break;
             }
             case K_PLACE: {
+                bool blocked = false;
                 for (int i = 0; i < len; i++) {
                     int ax = x+i*(dir==0);
                     int ay = y+i*(dir==1);
-                    map[ay*BOARD_W+ax] = CS_Ship;
+                    if (board[ay*BOARD_W+ax] == CS_Ship) {
+                        blocked = true;
+                        break;
+                    }
                 }
-                len_i += 1;
+                if (blocked) break;
+                for (int i = 0; i < len; i++) {
+                    int ax = x+i*(dir==0);
+                    int ay = y+i*(dir==1);
+                    board[ay*BOARD_W+ax] = CS_Ship;
+                }
+                len_i++;
                 len = ship_len[len_i];
                 break;
             }
@@ -122,5 +135,91 @@ void get_input(CellState_t map[BOARD_W * BOARD_H]) {
     }
 }
 
-void game_loop(CellState_t p1[BOARD_W * BOARD_H], (CellState_t p2[BOARD_W * BOARD_H) {
+
+void draw_board(int player, CellState_t board[BOARD_W * BOARD_H]) {
+    clrscr();
+    for (int x = 0; x < 10; x++) {
+        for (int y = 0; y < 10; y++) {
+            gotoxy(x*2, y);
+            switch (board[y*BOARD_W+x]) {
+                case CS_Water: case CS_Ship: printf("."); break;
+                case CS_WaterHit: printf("-"); break;
+                case CS_ShipHit: printf("X"); break;
+                default: printf("?"); break;
+            }
+        }
+    }
+    gotoxy(20, 0);
+    printf("P%d", player);
+}
+
+int player_turn(int player, CellState_t board[BOARD_W * BOARD_H]) {
+    int x = 0;
+    int y = 0; 
+    int k = 0;
+
+    draw_board(player, board);
+    gotoxy(0, 0);
+    printf("O"); 
+
+    while (k != K_PLACE) {
+        k = getk();
+        bool moved = (k == K_UP || k == K_DOWN || k == K_LEFT || k == K_RIGHT);
+        if (moved) {
+            gotoxy(x*2, y);
+            switch (board[y*BOARD_W+x]) {
+                case CS_Water: case CS_Ship: printf("."); break;
+                case CS_WaterHit: printf("-"); break;
+                case CS_ShipHit: printf("X"); break;
+                default: printf("?"); break;
+            }
+        }
+        switch (k) {
+            case K_EXIT:
+                clrscr();
+                printf("Bye!\n");
+                exit(0);
+            case K_UP:    if (y > 0) y--; break;
+            case K_DOWN:  if (y+1 < BOARD_H) y++; break;
+            case K_LEFT:  if (x > 0) x--; break;
+            case K_RIGHT: if (x+1 < BOARD_W) x++; break;
+        }
+        if (moved) { gotoxy(x*2, y); printf("O"); }
+    }
+
+    gotoxy(x*2, y);
+    int h = 0;
+    if (board[y*BOARD_W+x] == CS_Ship) {
+        board[y*BOARD_W+x] = CS_ShipHit;
+        printf("X");
+        h = 1;
+    } else {
+        board[y*BOARD_W+x] = CS_WaterHit;
+        printf("-");
+    }
+    while (!getk());
+    return 0;
+}
+
+void game_loop(CellState_t p1[BOARD_W * BOARD_H], CellState_t p2[BOARD_W * BOARD_H], int totalhits) {
+    int player, p1_hits, p2_hits;
+    int winner = -1;
+
+    while (winner == -1) {
+        player = !player;
+        switch (player) {
+            case 0: 
+                p1_hits += player_turn(1, p2);
+                if (p1_hits == totalhits) winner = 1;
+                break;
+            case 1: 
+                p2_hits += player_turn(2, p1);
+                if (p2_hits == totalhits) winner = 2;
+                break;
+            default: clrscr(); printf("ERROR\n"); exit(1);
+        }
+    }
+
+    clrscr();
+    printf("Speler 2 heeft gewonnen!\n");
 }
